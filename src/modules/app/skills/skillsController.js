a521f3a788f47643118f67a75b59b23840aa36e8
Corey White <smortopahri@gmail.com>
2015-03-31 01:03:43 -0400

    Starting to add skills content

--- src/modules/app/skills/skillsController.js
+++ src/modules/app/skills/skillsController.js
@@ -4,15 +4,46 @@ module.exports = /*@ngInject*/
   function skillsController($scope) {
 
     //Skills list
-    $scope.skills = ['Desktop GIS', 'Surveying', 'Frontend Web Development', 'Backend Web Development', 'Database Administration', 'DevOps'];
+    $scope.skills = [
+      {
+        name:'Desktop GIS',
+        description: 'Throughout both my educational and professional career I have mostly used Esri products in the GIS desktop setting. However, I have also used both GRASS, QGIS and other open source desktop tools on various projects ',
+        images: [
+          {
+            text: 'Least Cost Path Analysis',
+            image: '../assets/images/gisdesk1.png'
+          }
+        ]
+      },
+      {
+        name: 'Surveying',
+        description: ''
+      },
+      {
+        name: 'Frontend Web Development',
+        description: ''
+      },
+      {
+        name: 'Backend Web Development',
+        description: ''
+      },
+      {
+        name: 'Database Administration',
+        description: '',
+      },
+      {
+        name: 'DevOps',
+        description: ''
+      }];
+
     //Init skill value
-    $scope.skill = 'Desktop GIS';
+    $scope.skill = $scope.skills[0];
 
     $scope.setSkill = function (type){
       $scope.skill = type;
     };
 
     $scope.isActive = function (type){
-      return $scope.skill === type;
+      return $scope.skill.name === type;
     };
 };
