e96ae7fc4e00a5446bdcaa26884f184024d18122
Corey White <smortopahri@gmail.com>
2015-04-29 23:59:53 -0400

    Added content

--- src/modules/app/skills/skillsController.js
+++ src/modules/app/skills/skillsController.js
@@ -4,20 +4,15 @@ module.exports = /*@ngInject*/
   function skillsController($scope) {
 
     //Skills list
-    $scope.skills = [
-      {
+    $scope.skills = [{
         name:'Desktop GIS',
-        description: 'Throughout both my educational and professional career I have mostly used Esri products in the GIS desktop setting. However, I have also used both GRASS, QGIS and other open source desktop tools on various projects at NCSU. One project that heavily utilized GRASS was  ',
+        description: 'Throughout both my educational and professional career I have mostly used Esri products in the GIS desktop setting, but also use GRASS, QGIS and other open source desktop tools on various projects at NCSU and at work. My first job in GIS involved digitizing all of the sidewalks in Asheville, NC and that quickly led to creating other large datasets from scratch for the city. By creating these datasets and form classes at NC State I have developed a strong understanding of how to produce quality data for large projects. These roles led me to a position where I know design programs to automate and increase efficancy for these types of processes. ',
         images: [
           {
             text: 'Least Cost Path Analysis',
             image: '/assets/images/gisdesk1.png'
           }
         ]
-      },
-      {
-        name: 'Surveying',
-        description: ''
       },
       {
         name: 'Frontend Web Development',
