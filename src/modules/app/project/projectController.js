30207d6f552f6290404668ca8204cb2b9b2b1073
Corey White <smortopahri@gmail.com>
2015-05-01 00:15:05 -0400

    Added floor plan map to project

--- src/modules/app/project/projectController.js
+++ src/modules/app/project/projectController.js
@@ -3,4 +3,15 @@
 module.exports = /*@ngInject*/
   function projectController($scope) {
 
+    angular.element(document).ready(function () {
+
+
+    var plottmap = L.map('plottmap').setView([36.022018, -78.902483], 21);
+    L.tileLayer('https://{s}.tiles.mapbox.com/v3/ctwhite.l4hma6jb/{z}/{x}/{y}.png', {
+      attribution: 'Plott',
+      maxZoom: 22
+  }).addTo(plottmap);
+
+ });
+
   };
